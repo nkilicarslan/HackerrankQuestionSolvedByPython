8e033ce847fcde4cc2499dd9cdfad9a22530f117
Necat Kılıçarslan <necatkilicarslann@gmail.com>
2023-09-21 14:37:13 +0300

    add problem lonelyInteger

--- lonely_integer.py
+++ lonely_integer.py
@@ -1,18 +1,23 @@
-def lonelyinteger(a):
+import os
 
+def lonelyinteger(a):
+    # create a dictionary to store the number of times a number appears
     dict_num = {}
     unique_elem = 0
 
+    # loop through the array and check if the number is in the dictionary
     for number in a:
         if number not in dict_num:
             dict_num[number] = 1
         else:
             dict_num[number] += 1
 
+    # loop through the dictionary and find the number appears once
     for number in dict_num:
         if dict_num[number] == 1:
             unique_elem = number
 
+    # return the number that appears once
     return unique_elem
 
 
